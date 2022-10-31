3cacf3701038c26fe2c745d28ee02aa041139d65
Yura_viper <yuramcpecs@gmail.com>
2022-10-31 16:57:33 +0100

    Implemented class 'selected' for each CurrencySelectItem

--- src/components/Converter/Currency_input/Currency_select/Currency_select_item.js
+++ src/components/Converter/Currency_input/Currency_select/Currency_select_item.js
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
 
 function CurrencySelectItem({ currencyName, which, handleCurrencyChange }){
-    const currentPairQuantity = useSelector((state) => state.currency.currentPairQuantity);
-    console.log(currentPairQuantity)
+    
+    const [currentPair, currentPairQuantity] = useSelector((state) => [state.currency.currentPair, state.currency.currentPairQuantity]);
+    let isSelected = ( (which === 1 && currencyName === currentPair[0]) || (which === 2 && currencyName === currentPair[1]) );
 
     return(
-        <li onClick={() => handleCurrencyChange({currencyName, which, quantity: which === 1 ? currentPairQuantity[1] : currentPairQuantity[0] })}>{currencyName}</li>
+        <li className={`${isSelected ? "selected" : ""}`} onClick={() => handleCurrencyChange({currencyName, which, quantity: which === 1 ? currentPairQuantity[1] : currentPairQuantity[0] })}>{currencyName}</li>
     );
 }
 
