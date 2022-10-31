1aec95d695039053cc6e77b98b5c4b2910653556
Yura_viper <yuramcpecs@gmail.com>
2022-10-31 20:21:10 +0100

    Added basic styling and layout to Converter

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -3,7 +3,7 @@ import CurrencyInput from "./Currency_input/Currency_input";
 function Converter({ handleCurrencyChange, handleCurrencyQuantityChange }){
     return(
         <div id="converter">
-            <h2>ConverterRR</h2>
+            <h2 id="converterTitle" className="title">Smart Convert</h2>
             <CurrencyInput which={1} handleCurrencyChange={handleCurrencyChange} handleCurrencyQuantityChange={handleCurrencyQuantityChange} />
             <CurrencyInput which={2} handleCurrencyChange={handleCurrencyChange} handleCurrencyQuantityChange={handleCurrencyQuantityChange} />
         </div>
