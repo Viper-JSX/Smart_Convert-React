77d44de74fe53d08647f957ad4057965c68c51d6
Yura_viper <yuramcpecs@gmail.com>
2022-10-31 16:15:31 +0100

    Implemented currency quantities displaying

--- src/components/Converter/Currency_input/Currency_input.js
+++ src/components/Converter/Currency_input/Currency_input.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import CurrencySelect from "./Currency_select/Currency_select";
 
 function CurrencyInput({ which, handleCurrencyChange, handleCurrencyQuantityChange }){
+    const currentPairQuantity = useSelector((state) => state.currency.currentPairQuantity)
+    //const [ currentCurrencyValue, setCurrentCurrencyValue ] = useState();
+    const currentCurrencyValue = which === 1 ? currentPairQuantity[0] : currentPairQuantity[1];
+
     return(
         <div>
             <CurrencySelect which={which} handleCurrencyChange={handleCurrencyChange} />
-            <input type="text" placeholder="Quantity" onChange={(event) => handleCurrencyQuantityChange({ event, which })} />
+            <input type="text" placeholder="Quantity" value={currentCurrencyValue} onChange={(event) => handleCurrencyQuantityChange({ event, which })} />
         </div>
     )
 }
--- src/components/Converter/Currency_input/Currency_select/Currency_select.js
+++ src/components/Converter/Currency_input/Currency_select/Currency_select.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import CurrencySelectItem from "./Currency_select_item";
 
 function CurrencySelect(){
-    
+    const currencyNames = useSelector((state) => Object.keys(state.currency.currencyRatesRelativeToDollar));
     return(
         <div>
             {
