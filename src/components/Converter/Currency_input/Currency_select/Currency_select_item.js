1fab159ac53f94fd6e0eecc7ec1d631f205710c1
Yura_viper <yuramcpecs@gmail.com>
2022-10-31 16:45:27 +0100

    Implemented saving of value of an  input during changing currency

--- src/components/Converter/Currency_input/Currency_select/Currency_select_item.js
+++ src/components/Converter/Currency_input/Currency_select/Currency_select_item.js
@@ -1,6 +1,11 @@
+import { useSelector } from 'react-redux';
+
 function CurrencySelectItem({ currencyName, which, handleCurrencyChange }){
+    const currentPairQuantity = useSelector((state) => state.currency.currentPairQuantity);
+    console.log(currentPairQuantity)
+
     return(
-        <div onClick={() => handleCurrencyChange({currencyName, which, quantity: 1 })}>{currencyName}</div>
+        <div onClick={() => handleCurrencyChange({currencyName, which, quantity: which === 1 ? currentPairQuantity[1] : currentPairQuantity[0] })}>{currencyName}</div>
     );
 }
 
