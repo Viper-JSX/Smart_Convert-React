4dbb81c5cfafc75f3a8c40950672c1b8f012798a
Yura_viper <yuramcpecs@gmail.com>
2022-11-01 16:25:43 +0100

    Created DonationPage component and added styles to it

---
+++ src/components/Donation_page/Donation_page.js
@@ -0,0 +1,9 @@
+function DonationPage(){
+    return(
+        <div id="donationPage">
+            <b>Donation</b>
+        </div>
+    );
+}
+
+export default DonationPage;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import Converter from "./Converter/Converter";
+import DonationPage from "./Donation_page/Donation_page";
 import Header from "./Header/Header";
 
 function Layout({ handleCurrencyChange, handleCurrencyQuantityChange }){
@@ -8,7 +9,7 @@ function Layout({ handleCurrencyChange, handleCurrencyQuantityChange }){
             <Header />
             <Routes>
                 <Route path="/" element={<Converter handleCurrencyChange={handleCurrencyChange} handleCurrencyQuantityChange={handleCurrencyQuantityChange} />} />
-                <Route path="/donation" element={<h2>Donation</h2>} />
+                <Route path="/donation" element={<DonationPage />} />
                 <Route path="/about" element={<h2>About</h2>} />
             </Routes>
         </div>
