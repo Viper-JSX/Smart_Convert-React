cfb111d80425bb3c448bb1724b2798bbafa1fcf2
Yura_viper <yuramcpecs@gmail.com>
2022-11-02 09:06:37 +0100

    Added app title to loading window

--- src/components/Loading_window/Loading_window.js
+++ src/components/Loading_window/Loading_window.js
@@ -3,6 +3,7 @@ import LoadingIcon from "./Loading_icon";
 function LoadingWindow({ loadingWindowRef }){
     return(
         <div id="loadingWindow" ref={loadingWindowRef}>
+            <h2 className="appTitleHeading">Smart Convert</h2>
             <LoadingIcon />
         </div>
     );
