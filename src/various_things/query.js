4ffc9fb58f24605274e297304145939c3a06291d
Yura_viper <yuramcpecs@gmail.com>
2022-11-02 10:25:47 +0100

    Tiny change in query.js

--- src/various_things/query.js
+++ src/various_things/query.js
@@ -2,4 +2,3 @@ import { currencyRatesRelativeToDollar } from "./currency_rates"
 
 const allAvaliableSymbols = Object.keys(currencyRatesRelativeToDollar);
 export const query = `https://api.apilayer.com/fixer/latest?base=USD&symbols=${allAvaliableSymbols.join(",")}`;
-console.log(query)
