342e521cf086c0129abe3086a8cbf85908daf038
Yura_viper <yuramcpecs@gmail.com>
2023-01-03 21:52:45 +0100

    Removed the array destructurization

--- src/components/Converter/Currency_input/Currency_select/Currency_select.js
+++ src/components/Converter/Currency_input/Currency_select/Currency_select.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 function CurrencySelect({ which, handleCurrencyChange }){
     const currencyNames = useSelector((state) => Object.keys(state.currency.currencyRatesRelativeToDollar));
-    const [currentPairQuantity] = useSelector((state) => [state.currency.currentPair, state.currency.currentPairQuantity]);
+    const [currentPairQuantity] = useSelector((state) => [state.currency.currentPairQuantity]);
 
     return(
         <select className="currencySelect" onChange={(event) => handleCurrencyChange({ event, which, quantity: which === 1 ? currentPairQuantity[1] : currentPairQuantity[0]  })}>
